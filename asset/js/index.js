27ab0b042b99f5be91650a391cd988486ab0985c
hridoysheikh15 <ridoysheikh15@gmail.com>
2025-07-02 19:01:25 +0600

    Add logo

--- asset/js/index.js
+++ asset/js/index.js
@@ -5,3 +5,14 @@ $(document).ready(function () {
     // title view animation
     $(".page-title").removeClass("onload");
 });
+
+// set preview image with scroll-up animation
+function setPreviewImg(get) {
+    let preview = $("#indi-img-preview");
+    let src = $(get).attr("src");
+
+    preview.attr("src", src);
+    preview.removeClass("scroll-up-anim");
+    void preview[0].offsetWidth;
+    preview.addClass("scroll-up-anim");
+}

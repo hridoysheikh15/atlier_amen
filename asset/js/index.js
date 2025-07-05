6886e0d5dccdf8dcef3a6e77294481bcbd16fde6
hridoysheikh15 <ridoysheikh15@gmail.com>
2025-07-05 13:58:43 +0600

    New page added

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,10 +1,25 @@
+$("#billing_address").hide();
+
 $(document).ready(function () {
     // Trigger the aos animation
     AOS.init();
 
     // title view animation
     $(".page-title").removeClass("onload");
+
+    // get billing address form
+    $("#billing_form").on("click", function(e) {
+        let checkbox = $("#billing_form")[0];
+
+        if (checkbox.checked) {
+            $("#billing_address").hide('slow');
+        } else {
+            $("#billing_address").show('slow');
+            
+        }
+    })
 });
+
 
 // set preview image with scroll-up animation
 function setPreviewImg(get) {

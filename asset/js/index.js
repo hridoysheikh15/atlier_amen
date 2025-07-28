c17123565b4348749e1e42607c9802c0fcaeafac
hridoysheikh15 <ridoysheikh15@gmail.com>
2025-07-28 16:39:19 +0600

    Add video and image both as preview

--- asset/js/index.js
+++ asset/js/index.js
@@ -8,31 +8,85 @@ $(document).ready(function () {
     $(".page-title").removeClass("onload");
 
     // get billing address form
-    $("#billing_form").on("click", function(e) {
+    $("#billing_form").on("click", function (e) {
         let checkbox = $("#billing_form")[0];
 
         if (checkbox.checked) {
             $("#billing_address").hide('slow');
         } else {
             $("#billing_address").show('slow');
-            
+
         }
     })
 
-    $("scroll-up-anim").on("click", function() {
+    $("scroll-up-anim").on("click", function () {
         let animate = $("animate-to-scroll")
 
         animate.removeClass("scroll-up-anim");
         void animate[0].offsetWidth;
         animate.addClass("scroll-up-anim");
     })
+
+    // $('.thumb').on('click', function () {
+    //     const type = $(this).data('type');
+    //     const video = $('#video_previewer');
+    //     const img = $('#img_previewer');
+    //     const youtube = $('#youtube_previewer');
+
+    //     // Remove active class from all, add to clicked
+    //     $('.thumb').removeClass('active');
+    //     $(this).addClass('active');
+
+    //     // Hide all previews with fade
+    //     $('.indi-img-preview').fadeOut(0, function () {
+    //         $(this).hide();
+    //     });
+
+    //     if (type == 'video') {
+    //         const src = $(this).find('video').attr('src');
+    //         video.attr('src', src).attr('hidden', false).show().fadeIn(300)[0].play();
+    //     } else if (type == 'img') {
+    //         const src = $(this).find('img').attr('src');
+    //         img.attr('src', src).attr('hidden', false).show().fadeIn(300);
+    //     } else if (type == 'youtube') {
+    //         const ytSrc = $(this).data('youtube') + "?autoplay=1";
+    //         youtube.attr('src', ytSrc).attr('hidden', false).show().fadeIn(300);
+    //     }
+
+    //     // Stop video and YouTube when switching
+    //     video[0].pause();
+    //     video.attr('src', '');
+    //     youtube.attr('src', '');
+    // });
+
+    $('#img_previewer').hide();
 });
 
 
 // set preview image with scroll-up animation
-function setPreviewImg(get) {
-    let preview = $("#indi-img-preview");
+function setPreviewImg(get, type) {
+    let preview = $(".indi-img-preview");
     let src = $(get).attr("src");
+
+    switch (type) {
+        case 'img':
+            $('#video_previewer').hide();
+            $('#img_previewer').show();
+            break;
+
+        case 'video':
+            $('#img_previewer').hide();
+            $('#video_previewer').show();
+            break;
+
+        default:
+            $('#video_previewer').hide();
+            $('#img_previewer').show();
+            break;
+    }
+
+    $(".indi-img-gallery div div div").removeClass("active");
+    $(get).parent().addClass("active");
 
     preview.attr("src", src);
     preview.removeClass("scroll-up-anim");
